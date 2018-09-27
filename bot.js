153c41206c584ed03b308c2654998f31ccef988c
KingGamer2 <40805903+KingGamer2@users.noreply.github.com>
2018-09-27 22:13:54 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -453,19 +453,6 @@ Role : __${ar[message.guild.id].role}__`)
 
 })
 
- client.on('message', message => {
-	   if(message.content.startsWith(`${prefix}invite`)){
-		   if(!message.channel.guild) return message.channel.send("This Command is Just For Servers!")
-		   var embed = new Discord.RichEmbed()
-		   .setTitle(">> ClickHere To Add" + `${client.user.username}` + " <<")
-		   .setURL("https://discordapp.com/oauth2/authorize?client_id=" + `${client.user.id}` + "&scope=bot&permissions=8")
-		   .setTimestamp()
-		   .setFooter(`Requested By | ${message.author.username}`)
-		   .setColor("RANDOM")
-		   message.channel.send(":white_check_mark: | Check Your DM! تم الأرسال بلخاص")
-		   message.author.send({embed})
-	   }
-   });
 
 
 
